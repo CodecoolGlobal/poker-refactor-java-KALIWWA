536f4599016f1817ffd2ad8f1938228ffba724b2
Michal Kalinowski <subaru.kali@gmail.com>
2019-06-24 21:27:57 +0200

    initialized tests

---
+++ tests/CardTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardTest {
+
+    @org.junit.jupiter.api.Test
+    void getValue() {
+        fail("this test has to be implemented yet");
+    }
+}
