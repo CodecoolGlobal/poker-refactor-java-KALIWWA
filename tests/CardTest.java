8a41f39dd61bd9502257d827249f08604fcb94b2
Michal Kalinowski <subaru.kali@gmail.com>
2019-06-25 11:50:51 +0200

    created tests with & without correct data

--- tests/CardTest.java
+++ tests/CardTest.java
@@ -1,9 +1,40 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CardTest {
 
-    @org.junit.jupiter.api.Test
-    void getValue() {
-        fail("this test has to be implemented yet");
+    @Test
+    void getValue_correctCapitalLetter() {
+        Card card = new Card("HK");
+        assertEquals(13, card.getValue());
     }
+
+    @Test
+    void getValue_correctSmallLetter() {
+        Card card = new Card("hk");
+        assertEquals(13, card.getValue());
+    }
+
+    @Test
+    void getValue_correctNumber() {
+        Card card = new Card("H3");
+        assertEquals(3, card.getValue());
+    }
+
+    @Test
+    void constructor_tooBigNumber() {
+        assertThrows(IllegalArgumentException.class, () -> new Card("H13"));
+    }
+
+    @Test
+    void constructor_tooSmallNumber() {
+        assertThrows(IllegalArgumentException.class, () -> new Card("H0"));
+    }
+
+    @Test
+    void constructor_withNull() {
+        assertThrows(NullPointerException.class, () -> new Card(null));
+    }
+
 }
--- tests/PokerClientTest.java
+++ tests/PokerClientTest.java
@@ -3,20 +3,38 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PokerClientTest {
-    private PokerClient pokerClient = new PokerClient("h3", "d2", "s3", "s4", "cq");
+//    private PokerClient pokerClient = new PokerClient("h3", "d2", "s3", "s4", "cq");
 
     @Test
     void highestCardIsMine_CorrectData() {
+        PokerClient pokerClient = new PokerClient("h3", "d2", "s3", "s4", "cq");
         assertTrue(pokerClient.highestCardIsMine("s3", "h4", "s5", "d6", "sj"));
     }
 
     @Test
     void highestCardIsMine_InvalidCardColor() {
+        PokerClient pokerClient = new PokerClient("h3", "d2", "s3", "s4", "cq");
         assertThrows(IllegalArgumentException.class , () -> pokerClient.highestCardIsMine("rf", "ss", "56", "v5", "zz"));
     }
 
     @Test
     void highestCardIsMine_InvalidCardNumber() {
+        PokerClient pokerClient = new PokerClient("h3", "d2", "s3", "s4", "cq");
         assertThrows(IllegalArgumentException.class, () -> pokerClient.highestCardIsMine("s99", "h44", "s55", "d67", "sj"));
+    }
+
+    @Test
+    void constructor_withNull() {
+        assertThrows(NullPointerException.class, () -> new PokerClient(null, null, null, null, null));
+    }
+
+    @Test
+    void constructor_InvalidColor() {
+        assertThrows(IllegalArgumentException.class, () -> new PokerClient("rf", "ss", "56", "v5", "zz"));
+    }
+
+    @Test
+    void constructor_InvalidNumber() {
+        assertThrows(IllegalArgumentException.class, () -> new PokerClient("s99", "h44", "s55", "d67", "sj"));
     }
 }
